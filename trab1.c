33a718e5a852847edc018e863e7dc1aa0f6ae30d
João Henrique Dib <112108662+joaohdib@users.noreply.github.com>
2024-08-22 10:27:47 -0300

    Create trab1.c
    trab 1 de inf1316, 22/10

---
+++ trab1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+
+  int vetor[10] = {0};
+  int statloc;
+  int pid = fork();
+  
+  if (pid != 0) {
+  
+    for (int i = 0; i< 10; i++) {
+      vetor[i]++;
+    }
+    waitpid(-1,&statloc, 0);
+    for (int i = 0; i < 10; i++) {
+        printf("posicao %d: %d\n", i, vetor[i]); 
+    }
+    exit(0);
+  }
+  else {
+    
+    for (int i = 0; i< 10; i++) {
+      vetor[i] -= 1;
+    }
+    
+        for (int i = 0; i < 10; i++) {
+        printf("posicao %d: %d\n", i, vetor[i]); 
+    }
+    exit(0);
+  
+  }
+  
+  
+
+return 0;
+}
+
+
+
