6686803430750a5c5e0c2bb846606a371a32f109
João Henrique Dib <112108662+joaohdib@users.noreply.github.com>
2024-08-27 09:50:54 -0300

    Update trab2.c

--- trab2.c
+++ trab2.c
@@ -1,35 +1,15 @@
 #include <stdio.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-
-  int pid = fork();
-  int statloc;
-  
-  while (1) {
-  type_prompt( ); 
-  read_command(command, parameters);
-  
-  if (pid != 0) {
-    waitpid(-1,&statloc, 0);
-    exit(0);
-  }
-  else {
     int i;
     for (i = 1; i < argc-1; i++)
     {
       (void) printf("%s%s", argv[i], " ");
     }
     (void) printf("%s%s", argv[argc-1], "\n");
-    execve (command, parameters, 0);
-  }
-  exit(0);
-  }
-return 0;
+    return 0;
 }
-
-//AINDA NAO FUNCIONANDO
-
-
